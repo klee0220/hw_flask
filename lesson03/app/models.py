c3ac219730872ff3aee6ceba43047d4004bd641b
klee0220 <kleeuz@gmail.com>
2023-11-06 20:01:19 +0500

    hw3

---
+++ lesson03/app/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), unique=True, nullable=False)
+    secondname = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+
+def __repr__(self):
+    return f'Users ({self.firstname}, {self.secondname}, {self.email})'
